6eba2c4b882897781537010169ddfef78effa3a4
Soockee <gamingsockee@gmail.com>
2021-06-15 16:00:07 +0200

    HTTP Request working, Request Body unfinished

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ mod routes;
 
 use mongo_db::DB;
 use warp::{Filter, Rejection};
-
+use simple_logger::SimpleLogger;
 type Result<T> = std::result::Result<T, error::Error>;
 type WebResult<T> = std::result::Result<T, Rejection>;
 
 
 #[tokio::main]
 async fn main() {
+    SimpleLogger::new().init().unwrap();
     let db = DB::init().await.unwrap();
     let user_routes = routes::user_routes(db).recover(error::handle_rejection);
+    
 
     warp::serve(user_routes)
         .run(([0, 0, 0, 0], 13331))
--- src/repository.rs
+++ src/repository.rs
@@ -9,7 +9,7 @@ use reqwest::Client;
 use warp::{http::StatusCode, reject, reply::json, Reply};
 
 use log::{info, trace, warn};
-use simple_logger::SimpleLogger;
+
 
 pub async fn list_users(db: DB) ->WebResult<impl Reply> {
     let users = db.fetch_users().await.map_err(|e| reject::custom(e))?;
@@ -36,50 +36,50 @@ pub async fn delete_user(id: String, db: DB)  -> WebResult<impl Reply> {
 pub async fn login(body: LoginRequest, db: DB) ->WebResult<impl Reply> {
     let login_response: LoginResponse;
     let auth_service_base_url;
-    let auth_service_endpoint;
-    SimpleLogger::new().init().unwrap();
+    
     match env::var("AUTH_SERVICE_URL") {
         Ok(val) => auth_service_base_url = val,
         Err(_) => return Err(reject::custom(LoginError)),
     }
-    match env::var("AUTH_SERVICE_LOGIN_ENDPOINT") {
-        Ok(val) => auth_service_endpoint = val,
-        Err(_) => return Err(reject::custom(LoginError)),
-    }
     log::warn!("BaseURL: {}",auth_service_base_url);
-    log::warn!("Endpoint: {}",auth_service_endpoint);
-
-    let request_url_test = reqwest::Url::parse(&auth_service_base_url).unwrap();
-    let request_url_test = request_url_test.join(&auth_service_endpoint).unwrap();
-    log::warn!("Parsed URL: {}", request_url_test.into_string());
-    match db.fetch_user_by_username(&body.username).await{
-        Ok(_) =>{
-            let _body_s = body.clone();
-                // TODO: Change Hardcoded URL
-                let request_url = reqwest::Url::parse(&auth_service_base_url).unwrap();
-                let request_url = request_url.join(&auth_service_endpoint).unwrap();
-                let val = json!(&body);
-                let client = Client::new();
-                let response = client.post(request_url)
-                                    .json(&val)
-                                    .send();
-                match response.await {
-                    Ok(success_response) => match success_response.json().await{
-                        Ok(success_json) => {
-                            login_response = success_json;
+    let auth_service_base_url_str: &str = auth_service_base_url.as_str();
+    log::warn!("BaseURL as str: {}",auth_service_base_url_str);
+    let request_url= reqwest::Url::parse(&auth_service_base_url_str);
+    match request_url{
+        Ok(url) =>{
+            log::warn!("Parsed URL: {}", url.as_str());
+            match db.fetch_user_by_username(&body.username).await{
+                Ok(_) =>{
+                    let _body_s = body.clone();
+                        // TODO: Change Hardcoded URL
+                        let val = json!(&body);
+                        let client = Client::new();
+                        let response = client.post(url)
+                                            .json(&val)
+                                            .send();
+                        match response.await {
+                            Ok(success_response) => match success_response.json().await{
+                                Ok(success_json) => {
+                                    login_response = success_json;
+                                }
+                                Err(_) => {
+                                    return Err(reject::custom(LoginError))
+                                },
+                            }
+                            Err(_) => {
+                                    return Err(reject::custom(LoginError))
+                            },
                         }
-                        Err(_) => {
-                            return Err(reject::custom(LoginError))
-                        },
-                    }
-                    Err(_) => {
-                            return Err(reject::custom(LoginError))
-                    },
                 }
-        }
-        Err(_) =>{
-                return Err(reject::custom(LoginError))
-        }
+                Err(_) =>{
+                        return Err(reject::custom(LoginError))
+                }
+            }
+       } 
+       Err(_) => {
+            return Err(reject::custom(LoginError))
+       }
     }
+    
     Ok(json(&login_response))
 }
