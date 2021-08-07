96cf0a219d879b2edd0fcbd9e07b900f6d9b5b01
Henrik R. Huller <henrik.huller@henrik-huller.de>
2021-08-07 21:57:33 +0200

    Remove bearer of the curse

--- src/main/java/thm/mic/ser/user_service/config/AuthenticationConfigConstants.java
+++ src/main/java/thm/mic/ser/user_service/config/AuthenticationConfigConstants.java
@@ -3,7 +3,7 @@ package thm.mic.ser.user_service.config;
 public class AuthenticationConfigConstants {
     public static final String SECRET = "WeWillNotAllowYouToBringYourPetArmadilloAlong";
     public static final long EXPIRATION_TIME = 864000000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer of the curse ";
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/user/register";
     public static final String LOGIN_URL = "/login";
